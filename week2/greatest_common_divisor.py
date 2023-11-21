56fe133397e69a910b147cdde868f4e0d897f607
digzom <dicksonmelo@gmail.com>
2023-11-20 22:58:16 -0300

    chore: add implementation to fund gcd

---
+++ week2/greatest_common_divisor.py
@@ -0,0 +1,13 @@
+# for integers, a and b, their greatest common divisor or gcd(a, b) is the largest 
+# integer d so that d divides a and b.
+
+best = 0
+a = 391884838299102931000
+b = 1653264
+
+for d in range(1, a+b):
+    if a % d == 0 and b % d == 0:
+        best = d
+
+
+print(best)
