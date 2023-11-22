91eb7327e986238e592f1b31b537f26a60841848
digzom <dicksonmelo@gmail.com>
2023-11-21 22:56:38 -0300

    chore: first try

---
+++ week2/performant_algorith_fibs.py
@@ -0,0 +1,14 @@
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    return memo[n]
+
+n = 10 
+
+result = fibonacci(n)
+
+print(f"The {n}th Fibonacci number is: {result}")
+
