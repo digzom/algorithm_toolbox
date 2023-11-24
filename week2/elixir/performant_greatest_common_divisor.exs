0bd5d7c1bf39b1acf73754d966cef94083383123
digzom <dicksonmelo@gmail.com>
2023-11-24 05:39:46 -0300

    add time measurement

--- week2/elixir/performant_greatest_common_divisor.exs
+++ week2/elixir/performant_greatest_common_divisor.exs
@@ -1,9 +1,16 @@
-defmodule FindGcd do
-  def calc(a, 0, _), do: a
+defmodule Gcd do
+  def find(a, 0), do: a
 
-  def calc(a, b) do
-    calc(b, rem(a, b))
+  def find(a, b) do
+    a_reminder = rem(a, b)
+    find(b, a_reminder)
   end
 end
 
-FindGcd.calc(3918848080, 1653264) |> IO.puts()
+time_before = Time.utc_now()
+
+Gcd.find(3918848080, 1653264) |> IO.puts()
+
+time_after = Time.utc_now()
+
+Time.diff(time_after, time_before, :millisecond) |> IO.puts()
--- week2/performant_greatest_common_divisor.py
+++ week2/performant_greatest_common_divisor.py
@@ -1,3 +1,5 @@
+import time
+
 def euclidian_gcd(a, b):
     if b == 0:
         return a
@@ -6,6 +8,12 @@ def euclidian_gcd(a, b):
 
     return euclidian_gcd(b, a_reminder)
 
+start_time = time.time()
 gcd = euclidian_gcd(3918848080, 1653264)
+end_time = time.time()
+
+time_diff = end_time - start_time
 
 print(gcd)
+print(time_diff)
+
